5217c55f6d7b19bea210237e6a5e3b60a5c6b49a
Alexander Jody Dela Cruz <qajldelacruz@tip.edu.ph>
2022-07-11 22:31:33 +0800

    Bug Fixes and Change Nav Bar

--- resources/views/usermanagement/users.blade.php
+++ resources/views/usermanagement/users.blade.php
@@ -22,39 +22,45 @@
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('css/styles.css') }}"  rel="stylesheet">
     <script src="{{ asset('js/welcome.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
 
 <body id="body-pd">
 
-    <header class="header" id="header">
-        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-    </header>
-    <div class="l-navbar" id="nav-bar">
-        <nav class="nav">
-            <div> 
-              <a href="#" class="nav_logo"> 
-              <span class="nav_logo-name">Expense Manager</span>
-              </a>
-                <div class="nav_list"> 
-                  <a href="dashboard" class="nav_link"> 
-                  <span class="nav_name">Dashboard</span> 
-                  </a> 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="dashboard">Expense Mananger</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         User Management
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="roles">Roles</a>
+          <a class="dropdown-item" href="users">Users</a>
 
-                  <span class="nav_link active" data-bs-toggle="collapse" data-bs-target="#User-collapse">User Management</span> 
-                  <div class="collapse" id="User-collapse">
-                    <li class="nav_link"><a href="roles" class="link-dark rounded">Roles</a></li>
-                    <li class="nav_link"><a href="users" class="link-dark rounded">Users</a></li>
-                  </div>
-                  <span class="nav_link" data-bs-toggle="collapse" data-bs-target="#Expense-collapse">Expense Management</span> 
-                  <div class="collapse" id="Expense-collapse">
-                    <li class="nav_link"><a href="expensescategories" class="link-dark rounded">Expense Categories</a></li>
-                    <li class="nav_link"><a href="expenses" class="link-dark rounded">Expenses</a></li>
-                  </div>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Expense Management
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="expensescategories">Expense Categories</a>
+          <a class="dropdown-item" href="expenses">Expenses</a>
 
-                  </div>
-            </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
-        </nav>
-    </div>
+        </div>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
     <!--Container Main start-->
 <div class="height-100 bg-light">
 <div class="container">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,14 +19,51 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
-
+    
     <!-- Bootstrap core CSS --> 
     <link href="{{ asset('css/styles.css') }}"  rel="stylesheet">
     <script src="{{ asset('js/welcome.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
+
   <body id="body-pd">
 
-    <header class="header" id="header">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="dashboard">Expense Mananger</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         User Management
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="roles">Roles</a>
+          <a class="dropdown-item" href="users">Users</a>
+
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Expense Management
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="expensescategories">Expense Categories</a>
+          <a class="dropdown-item" href="expenses">Expenses</a>
+
+        </div>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
+<!--
+  <header class="header" id="header">
     <div class="header_toggle">  <i class='bx bx-menu' id="header-toggle"></i></div>
     </header>
     <div class="l-navbar" id="nav-bar">
@@ -56,8 +93,12 @@
             </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
         </nav>
     </div>
+  <div class="height-100 bg-light">
+
+</div>
+  -->
     <!--Container Main start-->
-<div class="height-100 bg-light">
+
       
   <div class="container">
         <br>
@@ -80,7 +121,7 @@
       @endforeach
     </tbody>
 </table>
-</div> 
+
 </div>
 </body>
 </html>
